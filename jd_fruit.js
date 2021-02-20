37f592faefc8fac9f770c15ec9a3b6173e6ecd1e
heiwoya <78598058+heiwoya@users.noreply.github.com>
2021-02-20 14:25:48 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -1,11 +1,12 @@
 /*
-东东水果:脚本更新地址 https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_fruit.js
-更新时间：2021-1-9
+东东水果:脚本更新地址 https://gitee.com/lxk0301/jd_scripts/raw/master/jd_fruit.js
+更新时间：2021-2-19
+活动入口：京东APP我的-更多工具-东东农场
 东东农场活动链接：https://h5.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 互助码shareCode请先手动运行脚本查看打印可看到
-一天只能帮助4个人。多出的助力码无效
+一天只能帮助3个人。多出的助力码无效
 ==========================Quantumultx=========================
 [task_local]
 #jd免费水果
